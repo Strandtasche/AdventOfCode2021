1967d7afe312093c3b4195d4b70b1aa9d41bdd70
Tobias Hornberger <saibot1207@googlemail.com>
2021-12-12 18:55:38 +0100

    day 04 part 1

---
+++ day04/day04.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def findWinner(boards, truthboards, numbers):
+    for n in numbers:
+        for index, b in enumerate(boards):
+            target_index = np.where(b == n)
+            truthboards[index][target_index] = True
+            winner = np.any(np.all(truthboards[index], axis=0)) or np.any(np.all(truthboards[index], axis=1))
+            if winner:
+                return boards[index], truthboards[index], n
+    print("no winner :(")
+
+
+if __name__ == "__main__":
+    with open("day04/input04.txt") as fp:
+        lines = fp.read().split("\n")
+
+    numbers = map(int, lines[0].split(","))
+    boards = []
+
+    for i in range(2, len(lines), 6):
+        tmp_board = [[int(a) for a in line.split()] for line in lines[i:i+5]]
+        boards.append(np.array(tmp_board, dtype=int))
+
+    tmp_truthboard = np.zeros((5, 5), dtype=bool)
+    truthboards = []
+    for i in range(len(boards)):
+        truthboards.append(tmp_truthboard.copy())
+
+    a, b, c = findWinner(boards, truthboards, numbers)
+    print(np.sum(a[~b]) * c)
+
+
+
