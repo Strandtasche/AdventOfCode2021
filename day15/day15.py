bb8deb6ae6ab34c220723f9169de7469414cbb66
Tobias Hornberger <saibot1207@googlemail.com>
2021-12-15 22:57:45 +0100

    day 15 part 2

--- day15/day15.py
+++ day15/day15.py
@@ -1,4 +1,5 @@
 import numpy as np
+np.set_printoptions(edgeitems=30, linewidth=100000)
 
 
 def part1(inpt):
@@ -30,16 +31,24 @@ def part1(inpt):
                     )
                     queue.append((element_x + x, element_y + y))
 
+def update_tiles(inpt):
+    news= np.tile(inpt, (5,5))
+    for j in range(1,5):
+        news[:, j*inpt.shape[1]:] += 1
+        news[j*inpt.shape[0]:, :] += 1
 
-
+    f =np.vectorize(lambda x: x if x <= 9 else x - 9)
+    return f(news)
 
 
 if __name__ == "__main__":
     with open("day15/input15.txt") as fp:
         lines = fp.read().split("\n")
 
-    chitons = np.zeros(shape=(len(lines), len(lines[0])))
+    chitons = np.zeros(shape=(len(lines), len(lines[0])), dtype=int)
     for i, line in enumerate(lines):
         chitons[i, :] = list(map(int, list(line)))
 
-    print(part1(chitons))
+    new_chitons = update_tiles(chitons)
+
+    print(part1(new_chitons))
